4fcb8c7f6c90c1d38a4a7ef6014528f51e966f8b
cambiph <incubusattax@gmail.com>
2019-09-10 09:29:51 +0200

    Set parentDir als default pad voor de nieuwe component

--- clone.sh
+++ clone.sh
@@ -1,55 +1,71 @@
 #!/bin/bash
-echo "Wat is de naam van de nieuwe webcomponent?"
 
-read naam
+####################
+# Helper functions #
+####################
 
-echo "Waar mag het project opgeslagen worden (Absoluut pad)?:"
+function determineDefaultPath() {
+    parentDir="$(dirname "${PWD}")"
+    fullPath=$parentDir/${naam}
+}
 
-read pad
+function overwriteDefaultPathWithInputIfPresent() {
+    if [[ -n $pad ]]; then
+        fullPath=$pad
+    fi
+}
 
-echo "Het project zal gekopieerd worden naar $pad/$naam. Is dit juist (Y/n)?"
-
-read keuze
+function readInput() {
+    read -p "Wat is de naam van de nieuwe webcomponent? " naam
+    determineDefaultPath
+    read -p "Waar mag het project opgeslagen worden? [$fullPath]: " pad
+    overwriteDefaultPathWithInputIfPresent
+}
 
 function replaceInFile() {
-    sed -i "" -e "s/blueprint/${naam}/g" $pad/$naam/$1
+    sed -i "" -e "s/blueprint/${naam}/g" $fullPath/$1
     if [[ $1 == "bamboo-specs/bamboo.yml" ]]; then
         NAAM=$(echo "$naam" | tr a-z A-Z)
-        sed -i "" -e "s/BLUEPRINT/${NAAM}/g" $pad/$naam/$1
+        sed -i "" -e "s/BLUEPRINT/${NAAM}/g" $fullPath/$1
     fi
     if [[ $1 == "vl-blueprint.src.js" ]]; then
         upper=`echo $naam|cut -c1|tr [a-z] [A-Z]`
         lower=`echo $naam|cut -c2-`
         NAAM=$upper$lower
-        sed -i "" -e "s/Blueprint/${NAAM}/g" $pad/$naam/$1
+        sed -i "" -e "s/Blueprint/${NAAM}/g" $fullPath/$1
     fi
 }
 
 function cleanUp() {
-    rm -rf $pad/$naam/$1
+    rm -rf $fullPath/$1
 }
 
 function rename() {
-    mv $pad/$naam/$1 $pad/$naam/$2
+    mv $fullPath/$1 $fullPath/$2
 }
 
-if [[ $keuze = "Y" ]]; then
-    mkdir -p $pad/$naam \
-    && cp -R . $pad/$naam/ \
-    && replaceInFile package.json \
-    && replaceInFile bamboo-specs/bamboo.yml \
-    && replaceInFile demo/vl-blueprint.html \
-    && replaceInFile vl-blueprint.src.js \
-    && cleanUp *bak \
-    && cleanUp test/*bak \
-    && cleanUp bamboo-specs/*bak \
-    && rename vl-blueprint.src.js vl-${naam}.src.js \
-    && rename demo/vl-blueprint.html demo/vl-${naam}.html \
-    && rename test/vl-blueprint.test.html test/vl-${naam}.test.html \
-    && echo "Kopieren voltooid!"
-else
-    echo "Start het script nogmaals met het correcte pad."
-    exit 1
-fi
+#####################
+# Application logic #
+#####################
+
+readInput
+
+mkdir -p $fullPath \
+&& rsync -arq . $fullPath --exclude .git --exclude node_modules --exclude README.md
+
+replaceInFile package.json
+replaceInFile bamboo-specs/bamboo.yml
+replaceInFile demo/vl-blueprint.html
+replaceInFile vl-blueprint.src.js
+
+cleanUp *bak
+cleanUp test/*bak
+cleanUp bamboo-specs/*bak
+
+rename vl-blueprint.src.js vl-${naam}.src.js
+rename demo/vl-blueprint.html demo/vl-${naam}.html
+rename test/vl-blueprint.test.html test/vl-${naam}.test.html
+
+echo "Project aangemaakt onder $fullPath"
 
 exit 0
