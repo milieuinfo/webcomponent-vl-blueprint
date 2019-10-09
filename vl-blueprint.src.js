2e51225f84df4ba4fe980a0b3574bf0c5cc1b337
Tom Coemans <tom.coemans@gmail.com>
2019-10-09 14:33:04 +0200

    script inladen verwijderd en define functie gebruikt

--- vl-blueprint.src.js
+++ vl-blueprint.src.js
@@ -1,20 +1,4 @@
-import { VlElement } from '/node_modules/vl-ui-core/vl-core.js';
-
-(() => {
-    loadScript('util.js', '../node_modules/@govflanders/vl-ui-util/dist/js/util.js', () => {
-        loadScript('blueprint.js', '../node_modules/@govflanders/vl-ui-blueprint/dist/js/blueprint.js')
-    });
-  
-    function loadScript(id, src, onload) {
-        if (!document.head.querySelector('#' + id)) {
-            let script = document.createElement('script');
-            script.setAttribute('id', id);
-            script.setAttribute('src', src);
-            script.onload = onload;
-            document.head.appendChild(script);
-        }
-    }
- })();
+import { VlElement, define } from '/node_modules/vl-ui-core/vl-core.js';
 
 /**
  * VlBlueprint
@@ -30,10 +14,6 @@ import { VlElement } from '/node_modules/vl-ui-core/vl-core.js';
  * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-blueprint.html|Demo}
  * 
  */
-export class VlBlueprint extends VlElement(HTMLElement) {
+export class VlBlueprint extends VlElement(HTMLElement) {}
 
-
-
-}
-
-customElements.define('vl-blueprint', VlBlueprint);
+define('vl-blueprint', VlBlueprint);
