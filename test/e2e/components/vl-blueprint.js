4e495b5a8b3868151d85d8cb4ff83f979f197525
Tom Coemans <tom.coemans@gmail.com>
2020-06-29 12:45:09 +0200

    removed duplicate constructor

--- test/e2e/components/vl-blueprint.js
+++ test/e2e/components/vl-blueprint.js
@@ -1,9 +1,5 @@
 const {VlElement} = require('vl-ui-core').Test;
 
-class VLBlueprint extends VlElement {
-  constructor(driver, selector) {
-    super(driver, selector);
-  }
-}
+class VLBlueprint extends VlElement {}
 
 module.exports = VLBlueprint;
