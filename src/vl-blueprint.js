abfe28da417b8796441f423a9bcf7c05d03b8e2b
Tom Coemans <tom.coemans@gmail.com>
2020-06-29 10:48:53 +0200

    removed JSDoc mixin

--- src/vl-blueprint.js
+++ src/vl-blueprint.js
@@ -6,7 +6,6 @@ import {vlElement, define} from '/node_modules/vl-ui-core/dist/vl-core.js';
  * @classdesc
  *
  * @extends HTMLElement
- * @mixin vlElement
  *
  * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-blueprint/releases/latest|Release notes}
  * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-blueprint/issues|Issues}
