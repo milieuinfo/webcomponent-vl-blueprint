e24649cdb9741328a09dec809128f75719e3dcd1
cambiph <philippe.cambien@gmail.com>
2020-08-07 08:00:10 +0200

    Fix logging

--- clone.js
+++ clone.js
@@ -117,7 +117,7 @@ async function setupGitRepo(options) {
     await git.add('./*');
     await git.commit('Automated commit by vl-blueprint');
     await git.addRemote('origin', `https://github.com/milieuinfo/${options.naam}.git`);
-    console.log(`Setup van nieuw project klaar! \n Lokale git repository is aangemaakt met remote https://github.com/milieuinfo/${options.naam}.git \n VERGEET NIET DE REPOSITORY OP GITHUB AAN TE MAKEN!\n Bedankt voor het vertrouwen in UIG!`);
+    console.log(`Setup van nieuw project klaar! \n Lokale git repository is aangemaakt met remote https://github.com/milieuinfo/webcomponent-vl-ui-${options.naam}.git \n VERGEET NIET DE REPOSITORY OP GITHUB AAN TE MAKEN!\n Bedankt voor het vertrouwen in UIG!`);
   } catch (e) {
     console.log(e);
   }
