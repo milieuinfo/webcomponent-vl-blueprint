abfac5f52ec1632b27ec4b42d7c3d5a71ab63558
cambiph <philippe.cambien@gmail.com>
2020-08-12 09:49:17 +0200

    Fix remote name

--- clone.js
+++ clone.js
@@ -116,7 +116,7 @@ async function setupGitRepo(options) {
     await git.init();
     await git.add('./*');
     await git.commit('Automated commit by vl-blueprint');
-    await git.addRemote('origin', `https://github.com/milieuinfo/${options.naam}.git`);
+    await git.addRemote('origin', `https://github.com/milieuinfo/webcomponent-vl-ui-${options.naam}.git`);
     console.log(`Setup van nieuw project klaar! \n Lokale git repository is aangemaakt met remote https://github.com/milieuinfo/webcomponent-vl-ui-${options.naam}.git \n VERGEET NIET DE REPOSITORY OP GITHUB AAN TE MAKEN!\n Bedankt voor het vertrouwen in UIG!`);
   } catch (e) {
     console.log(e);
