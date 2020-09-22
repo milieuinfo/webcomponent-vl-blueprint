2a488f72ad347fa46b7942351b3f9b7a4107c7a8
Tom Coemans <tom.coemans@gmail.com>
2020-09-22 14:08:32 +0200

    ESlint

--- util/cloneUtils.js
+++ util/cloneUtils.js
@@ -1,6 +1,6 @@
 const fs = require('fs').promises;
 const path = require('path');
-const { setupGitRepo } = require('./gitUtils');
+const {setupGitRepo} = require('./gitUtils');
 
 async function initializeWebcomponent(options) {
   try {
@@ -14,19 +14,19 @@ async function initializeWebcomponent(options) {
     await replaceInFile(path.resolve(savePath, 'README.md.template'), options.name);
     await replaceInFile(path.resolve(savePath, 'index.js'), options.name);
     await replaceInFile(path.resolve(savePath, 'src/index.js'), options.name);
-    
+
     await replaceInFile(path.resolve(savePath, 'bamboo-specs/bamboo.yml'), options.name);
     await replaceInFile(path.resolve(savePath, 'demo/vl-blueprint.html'), options.name);
     await replaceInFile(path.resolve(savePath, 'test/e2e/components/vl-blueprint.js'), options.name);
     await replaceInFile(path.resolve(savePath, 'test/e2e/pages/vl-blueprint.page.js'), options.name);
     await replaceInFile(path.resolve(savePath, 'test/e2e/blueprint.test.js'), options.name);
     await replaceInFile(path.resolve(savePath, 'test/unit/vl-blueprint.test.html'), options.name);
-    
+
     await replace(path.resolve(savePath, 'test/e2e/pages/vl-blueprint.page.js'), 'Config.baseUrl', `Config.baseUrl + '/demo/vl-${options.name}.html'`);
-    
+
     await rename(path.resolve(savePath, 'src/vl-blueprint.js'), path.resolve(savePath, `src/vl-${options.name}.js`));
     await rename(path.resolve(savePath, 'README.md.template'), path.resolve(savePath, 'README.md'));
-    
+
     await rename(path.resolve(savePath, 'demo/vl-blueprint.html'), path.resolve(savePath, `demo/vl-${options.name}.html`));
     await rename(path.resolve(savePath, 'test/e2e/components/vl-blueprint.js'), path.resolve(savePath, `test/e2e/components/vl-${options.name}.js`));
     await rename(path.resolve(savePath, 'test/e2e/pages/vl-blueprint.page.js'), path.resolve(savePath, `test/e2e/pages/vl-${options.name}.page.js`));
@@ -58,7 +58,7 @@ async function replaceDescriptionInPackageJson(path, description) {
 
 async function replaceInFile(path, name) {
   const nameLowercase = name.toLowerCase();
-  const nameCamelcaseZonderDashes = await nameLowercase.replace(/(^|[\s-])\S/g, function (match) {
+  const nameCamelcaseZonderDashes = await nameLowercase.replace(/(^|[\s-])\S/g, function(match) {
     return match.toUpperCase();
   }).replace(/-/g, '');
   const nameUppercaseZonderDashes = await nameLowercase.toUpperCase().replace(/-/g, '');
@@ -81,17 +81,17 @@ async function replace(someFile, search, replacement) {
 }
 
 async function copyFolder(from, to, exclusions) {
-    await fs.mkdir(to);
-    const dirs = await fs.readdir(from);
-    for (let index = 0; index < dirs.length; index++) {
-      if (!exclusions.includes(dirs[index])) {
-        if ((await (await fs.lstat(path.join(from, dirs[index]))).isFile())) {
-          await fs.copyFile(path.join(from, dirs[index]), path.join(to, dirs[index]));
-        } else {
-          await copyFolder(path.join(from, dirs[index]), path.join(to, dirs[index]), exclusions);
-        }
+  await fs.mkdir(to);
+  const dirs = await fs.readdir(from);
+  for (let index = 0; index < dirs.length; index++) {
+    if (!exclusions.includes(dirs[index])) {
+      if ((await (await fs.lstat(path.join(from, dirs[index]))).isFile())) {
+        await fs.copyFile(path.join(from, dirs[index]), path.join(to, dirs[index]));
+      } else {
+        await copyFolder(path.join(from, dirs[index]), path.join(to, dirs[index]), exclusions);
       }
     }
+  }
 }
 
-module.exports = { initializeWebcomponent };
+module.exports = {initializeWebcomponent};
